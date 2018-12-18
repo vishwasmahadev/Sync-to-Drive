40e762aa295705cf199102840bd3eb8e4f19554b
Vishwas Mahadev <vishwas.n17@gmail.com>
2018-12-18 15:52:19 -0500

    Update testing.py
    updated the zipping file code

--- testing.py
+++ testing.py
@@ -5,8 +5,11 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from authentication import Auth
 import logging
+import zipfile
+import os
 
 def main():
+
     logging.basicConfig(filename='runner.log', filemode='a+',format='%(levelname)s -> %(asctime)s: %(message)s', level=logging.DEBUG)
     auth=Auth().authenticate('https://www.googleapis.com/auth/drive')
 
@@ -16,8 +19,19 @@ def main():
         responseFileName=res.get('name')
         logging.info(responseFileName)
     finally:
-        print("In finally code")
+      print("In finally code")
+
+##    file_path=[]
+##    for root,folder,file in os.walk(r"C:\Users\vishwas.m\Desktop\API Gateway\Sample Runbook"):
+##        for files in file:
+##                file_path.append(os.path.join(root,files))
+##    print(file_path)
+##    with zipfile.ZipFile(r'C:\Users\vishwas.m\Desktop\API Gateway\Sample Runbook\zipped.zip','w') as zip:
+##      for file in file_path:
+##          zip.write(file)
 
 if __name__=='__main__':
     main()
+
+
 
